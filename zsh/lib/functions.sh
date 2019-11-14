dc9cbb6ce727ddf2341c82fb68e7df3678049d09
Brian Sunter <brian@briansunter.com>
2019-11-13 19:58:01 -0800

    add terminal color

--- zsh/lib/functions.sh
+++ zsh/lib/functions.sh
@@ -15,3 +15,16 @@ vf() {
   IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
   [[ -n "$files" ]] && nvim "${files[@]}"
 }
+
+# ls extensions
+export CLICOLOR=1
+ls_ext() {
+  if [ $# -eq 0 ]; then # if ls is called without any arguments
+    # make ls print full info, with color
+    ls -laF
+  else
+    ls $@ # otherwise call as intended
+  fi
+}
+
+alias ls="ls_ext"
