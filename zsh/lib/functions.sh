04e6d3bc76fb587f1283ba0c2bb5f4932044f191
Brian Sunter <brian@briansunter.com>
2019-12-29 16:56:36 -0800

    add git gpg setup

--- zsh/lib/functions.sh
+++ zsh/lib/functions.sh
@@ -86,3 +86,8 @@ function init-git-secrets () {
     git secrets --register-aws
 }
 
+function setup-git-gpg () {
+keybase pgp export | gpg --import
+keybase pgp export --secret | gpg --allow-secret-key-import --import
+}
+
